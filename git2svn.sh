1df34111e1627de00d675b3ebe241c932bb44dd5
codedsk <telldsk@gmail.com>
2016-10-19 21:47:33 -0400

    handle older subversion 1.6 versions
    added code to handle subversion 1.6 where they put a .svn directory in
    each of the directories in the repository. after removing all data from
    the subversion repository and copying files from the git repository, we
    need to rebuild the .svn directories in the previously established directories.
    
    added a verbose flag.
    
    added ability to specify a subversion repository name instead of a full url.
    
    set the default location for temporary files to the caller's home
    directory.

--- git2svn.sh
+++ git2svn.sh
@@ -5,49 +5,91 @@
 # git2svn.sh \
 #   -g https://github.com/user/myrepo.git \
 #   -s https://hubzero.org/tools/myrepo/svn/trunk \
-#   -c /www/hub/hubconfiguration.php
+#   -c /www/hub
+#
+# if an svn repository url is not provided, but a repo name is,
+# then use hubconfiguration.php to guess at the svn repository url
+# git2svn.sh \
+#   -g https://github.com/user/myrepo.git \
+#   -s myrepo
+#   -c /www/hub
 
+
+function debug {
+    if [[ "${verbose}" == "True" ]] ; then
+        echo "$1";
+    fi
+}
+
+function error {
+    echo "ERROR: $1";
+    exit 1;
+}
 
 function svn_checkin {
-    echo '... adding files'
-    for file in `svn st ${svn_dir} | awk -F" " '{print $1 "|" $2}'`; do
-        fstatus=`echo ${file} | cut -d"|" -f1`
-        fname=`echo ${file} | cut -d"|" -f2`
+    debug '... adding files to the Subversion repository';
+    # keep looking until we have taken care of all 
+    while [ `svn st ${svn_dir} | awk -F" " '{print $1 "|" $2}' | grep '[?!~]|' | wc -l` != "0" ] ; do
+        for file in `svn st ${svn_dir} | awk -F" " '{print $1 "|" $2}'`; do
+            fstatus=`echo ${file} | cut -d"|" -f1`;
+            fname=`echo ${file} | cut -d"|" -f2`;
 
-        if [ "${fstatus}" == "?" ]; then
-            if [[ "${fname}" == *@* ]]; then
-                svn add $fname@;
-            else
-                svn add ${fname};
+            if [ "${fstatus}" == "?" ]; then
+                if [[ "${fname}" == *@* ]]; then
+                    svn ${svn_opts} add $fname@;
+                else
+                    svn ${svn_opts} add ${fname};
+                fi
             fi
-        fi
-        if [ "${fstatus}" == "!" ]; then
-            if [[ "${fname}" == *@* ]]; then
-                svn rm $fname@;
-            else
-                svn rm ${fname};
+            if [ "${fstatus}" == "!" ]; then
+                if [[ "${fname}" == *@* ]]; then
+                    svn ${svn_opts} rm $fname@;
+                else
+                    svn ${svn_opts} rm ${fname};
+                fi
             fi
-        fi
-        if [ "${fstatus}" == "~" ]; then
-            rm -rf ${fname};
-            svn up ${fname};
-        fi
+            if [ "${fstatus}" == "~" ]; then
+                if [[ -d "${fname}" ]] ; then
+                    # svn 1.6 keeps a .svn directory in every directory
+                    # if fname was a directory we removed before we copied
+                    # files from the git repo, we need to put the .svn directory
+                    # back. checkout the directory with --depth immediate
+                    # use POSIX shell foo to strip off the beginning file path
+                    fname_t=`readlink -f ${fname}`;
+                    svn_dir_t=`readlink -f ${svn_dir}`;
+                    svn_fname=${fname_t#${svn_dir_t}};
+                    svn ${svn_opts} checkout ${svn_repo_url}/${svn_fname} --depth immediates --force ${fname};
+                    unset fname_t;
+                    unset svn_dir_t;
+                    unset svn_fname;
+                else
+                    # otherwise, remove the file and 
+                    # pull the old file from the svn repo
+                    rm -rf ${fname};
+                    svn ${svn_opts} up ${fname};
+                fi
+            fi
+        done
     done
-    echo '... finished adding files'
+    debug '... finished adding files to the Subversion repository';
 }
 
 function svn_commit {
-    echo "... committing -> [${author}]: ${msg}";
-    cd ${svn_dir} && svn ${svn_auth} commit -m "[${author}]: ${msg}" && cd ${base_dir};
-    echo '... committed!'
+    debug "... committing to Subversion -> [${author}]: ${msg}";
+    cd ${svn_dir} && svn ${svn_opts} ${svn_auth} commit -m "[${author}]: ${msg}" && cd ${base_dir};
+    debug '... committed!';
 }
 
-
-git_repo_url=""
-svn_repo_url=""
-project_name=`mktemp -u XXXXXXXXXX`
-hubconfig=""
-options=":c:g:p:s:"
+git_repo_url="";
+svn_repo_url="";
+hubconfig=".";
+verbose="False";
+t_repos_base="${HOME}";
+svn_opt_quiet="-q";
+svn_opts="";
+git_opt_quiet="-q";
+git_opts="";
+options=":c:g:p:r:s:v";
 
 # parse the command line flags and options
 # separate flags from options
@@ -57,6 +99,11 @@ let nUnnamedArgs=0
 while (( "$#" ))
 do
    case $1 in
+      -v )
+           namedArgs[${nNamedArgs}]=$1
+           let nNamedArgs++
+           shift
+           ;;
       -* )
            namedArgs[${nNamedArgs}]=$1
            let nNamedArgs++
@@ -78,10 +125,53 @@ do
    case ${Option} in
       c ) hubconfig=${OPTARG};;
       g ) git_repo_url=${OPTARG};;
-      p ) project_name=${OPTARG};;
+      r ) t_repos_base=${OPTARG};;
       s ) svn_repo_url=${OPTARG};;
+      v ) verbose="True";;
    esac
 done
+
+
+# exit immediately on error
+set -e
+
+# add quiet flag (-q) to subversion operations
+if [[ "${verbose}" == "False" ]] ; then
+    svn_opts="${svn_opts} ${svn_opt_quiet}";
+    git_opts="${git_opts} ${git_opt_quiet}";
+fi
+
+# input validation
+
+# regexp only works with ascii urls
+url_regex='^https?://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$';
+proj_regex='^[-A-Za-z0-9\+@#/%?=~_|!:,.]+$';
+
+if [[ ! -d "${t_repos_base}" ]] ; then
+    error "Temporary repository directory \"${t_repos_base}\" does not exist.";
+fi
+
+if [[ ! -r "${hubconfig}/hubconfiguration.php" ]] ; then
+    error "Hub configuration file \"${hubconfig}/hubconfiguration.php\" is not readable";
+else
+    hubconfig="${hubconfig}/hubconfiguration.php";
+fi
+
+if [[ ! "${git_repo_url}" =~ ${url_regex} ]] ; then
+    error "Git repository does not look like a url: ${git_repo_url}";
+fi
+
+if [[ ! "${svn_repo_url}" =~ ${url_regex} ]] ; then
+    if [[ ! "${svn_repo_url}" =~ ${proj_regex} ]] ; then
+        error "Subversion repository does not look like a url: ${svn_repo_url}";
+    else
+        # looks like the svn_repo_url is a project name
+        # try to guess the repository name from forgeURL
+
+        forgeURL=`grep forgeURL ${hubconfig} | sed -n "s/^.*'\(.*\)'.*;/\1/p"`;
+        svn_repo_url="${forgeURL}/tools/${svn_repo_url}/svn/trunk";
+    fi
+fi
 
 # grab the svn username and password from the hub configuration file.
 # these regexps work as long as the strings are single quoted.
@@ -90,36 +180,41 @@ svn_pass=`grep svn_password ${hubconfig} | sed -n "s/^.*'\(.*\)'.*;/\1/p"`;
 svn_auth="--username ${svn_user} --password ${svn_pass}";
 
 base_dir=`pwd`;
-repos_dir=`mktemp -d -p . contribtool.XXXXXXXXXX`;
-git_dir="${repos_dir}/${project_name}.github";
-svn_dir="${repos_dir}/${project_name}.hubzero";
-
+repos_dir=`mktemp -d -p ${t_repos_base} git2svn-tmp-XXXXXXXXXX`;
+git_dir="${repos_dir}/git.repo";
+svn_dir="${repos_dir}/svn.repo";
 
 # clone the git repository
-git clone -q ${git_repo_url} ${git_dir};
+debug "cloning the Git repository ${git_repo_url}";
+git clone ${git_opts} "${git_repo_url}" ${git_dir};
 
 # check out the svn repository
-svn checkout -q ${svn_repo_url} ${svn_dir};
+debug "checking out the Subversion repository ${svn_repo_url}";
+svn ${svn_opts} checkout "${svn_repo_url}" ${svn_dir};
 
 # find the latest commit in the git repo
 commit=`cd ${git_dir} && git rev-list --all -n 1 && cd ${base_dir}`;
 
-echo "Committing ${commit}...";
+debug "querying author information from Git commit ${commit}";
 author=`cd ${git_dir} && git log -n 1 --pretty=format:%an ${commit} && cd ${base_dir}`;
 msg=`cd ${git_dir} && git log -n 1 --pretty=format:%s ${commit} && cd ${base_dir}`;
 
 # Checkout the current commit on git
-echo '... checking out commit on Git'
-cd ${git_dir} && git checkout -q ${commit} && cd ${base_dir};
+debug "... checking out Git commit ${commit}";
+cd ${git_dir} && git checkout ${git_opts} ${commit} && cd ${base_dir};
 
 # Delete everything from SVN and copy new files from Git
 # Keep the bare minimum directory structure for hubzero applications
-echo '... copying files'
+debug "... copying files from Git to Subversion repository";
 rm -rf ${svn_dir}/*;
 cp -prf ${git_dir}/* ${svn_dir}/;
-mkdir -p ${svn_dir}/bin ${svn_dir}/data ${svn_dir}/doc \
-         ${svn_dir}/examples ${svn_dir}/middleware \
-         ${svn_dir}/rappture ${svn_dir}/src;
+mkdir -p ${svn_dir}/bin \
+         ${svn_dir}/data \
+         ${svn_dir}/doc \
+         ${svn_dir}/examples \
+         ${svn_dir}/middleware \
+         ${svn_dir}/rappture \
+         ${svn_dir}/src;
 
 
 # Remove Git specific files from SVN
@@ -129,8 +224,12 @@ do
 done
 
 # Add new files to SVN and commit
-svn_checkin
+svn_checkin;
 svn_commit;
 
 # cleanup temp directories
-rm -rf ${repos_dir}
+debug "cleaning up temporary directory ${repos_dir}";
+rm -rf ${repos_dir};
+
+debug "exiting...";
+exit 0;
